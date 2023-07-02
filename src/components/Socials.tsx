7013db25d95cdea56e646ab5eeffe32ec2050c23
MatejBendik <matejbendik.mb@gmail.com>
2023-07-02 11:48:34 +0200

    Add array for socials

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -1,34 +1,45 @@
 import ButtonWithIcon from "./ButtonWithIcon";
 
+const socials = [
+  {
+    href: "https://twitter.com/BendikMatej",
+    name: "Twitter",
+    icon: "twitter.webp",
+    followers_count: 12109,
+  },
+  {
+    href: "https://www.linkedin.com/in/matejbendik/",
+    name: "linkedIn",
+    icon: "linkedin.webp",
+    followers_count: 269,
+  },
+  {
+    href: "https://www.github.com/MatejBendik",
+    name: "GitHub",
+    icon: "github.webp",
+    followers_count: 217,
+  },
+  {
+    href: "https://www.instagram.com/matejbendik",
+    name: "Instagram",
+    icon: "instagram.webp",
+    followers_count: 1869,
+  },
+];
+
 const Socials = () => {
   return (
     <section className="mb-12">
       <h2 className="text-center text-[1.7rem] font-semibold mb-3">Socials</h2>
       <div className="flex flex-col gap-4">
-        <ButtonWithIcon
-          href="https://twitter.com/BendikMatej"
-          name="Twitter"
-          icon="twitter.webp"
-          followers_count={12109}
-        />
-        <ButtonWithIcon
-          href="https://www.linkedin.com/in/matejbendik/"
-          name="linkedIn"
-          icon="linkedin.webp"
-          followers_count={269}
-        />
-        <ButtonWithIcon
-          href="https://www.github.com/MatejBendik"
-          name="GitHub"
-          icon="github.webp"
-          followers_count={217}
-        />
-        <ButtonWithIcon
-          href="https://www.instagram.com/matejbendik"
-          name="Instagram"
-          icon="instagram.webp"
-          followers_count={1869}
-        />
+        {socials.map((social) => (
+          <ButtonWithIcon
+            href={social.href}
+            name={social.name}
+            icon={social.icon}
+            followers_count={social.followers_count}
+          />
+        ))}
       </div>
     </section>
   );
