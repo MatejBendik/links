6d75dd78ee3cc08709935f782bea6d5291d601b2
MatejBendik <matejbendik.mb@gmail.com>
2024-11-20 23:02:05 +0100

    Update numbers

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -11,7 +11,7 @@ const socials = [
     href: "https://www.threads.net/@matejbendik",
     name: "Threads",
     icon: "threads.webp",
-    followers_count: 813,
+    followers_count: 814,
   },
   {
     href: "https://www.linkedin.com/in/matejbendik/",
